eaf9ab420ba243e7c93062056669d77fa62a3af9
Frank Hereford <frank@frankhereford.com>
2024-12-03 10:51:05 -0600

    maybe working part 2?

--- day_03.py
+++ day_03.py
@@ -5,7 +5,7 @@ import re
 multiplication_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
 
 sum_of_products = 0
-with open("input/03/real_input", "r") as file:
+with open("input/03/test_input", "r") as file:
     safe_count = 0
     for line in file:
         # print(line)
@@ -13,5 +13,28 @@ with open("input/03/real_input", "r") as file:
         for match in matches:
             # print(match)
             sum_of_products += int(match.group(1)) * int(match.group(2))
+
+# print(f"Sum of products: {sum_of_products} ✅")
+
+# zwa_multiplication_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
+
+remove_nonexecutable_lines = re.compile(r"(^|do\(\))(.*?)(don't\(\)|$)")
+
+sum_of_products = 0
+with open("input/03/part_b_test_input", "r") as file:
+    safe_count = 0
+    for line in file:
+        print(line)
+        matches = remove_nonexecutable_lines.finditer(line.strip())
+        for match in matches:
+            # print("new iteration:")
+            # print(match)
+            # print(match.group(1))
+            # print(match.group(2))
+            # print(match.group(3))
+            inner_matches = multiplication_pattern.finditer(match.group(2))
+            for inner_match in inner_matches:
+                print(inner_match)
+                sum_of_products += int(inner_match.group(1)) * int(inner_match.group(2))
 
 print(f"Sum of products: {sum_of_products} ✅")
