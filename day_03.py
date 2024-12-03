fda639626fd7f95e0f6832140555c249ccdaf3d7
Frank Hereford <frank@frankhereford.com>
2024-12-03 10:54:00 -0600

    tidy up

--- day_03.py
+++ day_03.py
@@ -14,24 +14,17 @@ with open("input/03/test_input", "r") as file:
             # print(match)
             sum_of_products += int(match.group(1)) * int(match.group(2))
 
-# print(f"Sum of products: {sum_of_products} ✅")
+print(f"Sum of products: {sum_of_products} ✅")
 
-# zwa_multiplication_pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-
-remove_nonexecutable_lines = re.compile(r"(^|do\(\))(.*?)(don't\(\)|$)")
+remove_nonexecutable_code = re.compile(r"(^|do\(\))(.*?)(don't\(\)|$)")
 
 sum_of_products = 0
-with open("input/03/part_b_test_input", "r") as file:
+with open("input/03/real_input", "r") as file:
     safe_count = 0
     for line in file:
         print(line)
-        matches = remove_nonexecutable_lines.finditer(line.strip())
+        matches = remove_nonexecutable_code.finditer(line.strip())
         for match in matches:
-            # print("new iteration:")
-            # print(match)
-            # print(match.group(1))
-            # print(match.group(2))
-            # print(match.group(3))
             inner_matches = multiplication_pattern.finditer(match.group(2))
             for inner_match in inner_matches:
                 print(inner_match)
