652fe3b6e984a7cbd7c9842367ae17fb03843f0b
Frank Hereford <frank@frankhereford.com>
2024-12-05 11:37:12 -0600

    maybe part 1

---
+++ day_05.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import re
+
+rule_pattern = re.compile(r"(\d+)\|(\d+)")
+
+rule_pairs = []
+updates = []
+
+
+def is_compliant(update, rule_pair):
+    try:
+        before_index = update.index(rule_pair[0])
+        after_index = update.index(rule_pair[1])
+        if before_index < after_index:
+            return True
+        return False
+    except:
+        # print("Non-applicable rule")
+        return True
+
+
+with open("input/05/test_input_1", "r") as file:
+    # with open("input/05/real_input", "r") as file:
+    input_rules = True
+    for line in file:
+        if line.strip():
+            if input_rules:
+                # print(line)
+                match = rule_pattern.search(line.strip())
+                page_before = int(match.group(1))
+                page_after = int(match.group(2))
+                # print(f"Page {page_before} comes before page {page_after}.")
+                rule_pairs.append((page_before, page_after))
+
+            else:  # updates
+                updated_pages = [int(x) for x in line.strip().split(",")]
+                # print(f"Updated pages: {updated_pages}")
+                updates.append(updated_pages)
+        else:
+            input_rules = False
+
+
+print(rule_pairs)
+print(updates)
+
+sum_of_middles = 0
+for update in updates:
+    update_ok = True
+    for rule in rule_pairs:
+        # print(f"Is compliant: {update}, {rule}, {is_compliant(update, rule)}")
+        if not is_compliant(update, rule):
+            update_ok = False
+            break
+    print(f"Update {update} compliance status: {update_ok}")
+    if update_ok:
+        middle_index = int((len(update) - 1) / 2)
+        print(f"Middle index: {middle_index}")
+        print(f"Middle page: {update[middle_index]}")
+        sum_of_middles += update[middle_index]
+print(f"Sum of middle pages: {sum_of_middles}")
