d5d7aaa83c9cf430be18585b199204d83e635c97
Frank Hereford <frank@frankhereford.com>
2024-12-08 16:15:42 -0600

    it was actually this easy

--- day_07.py
+++ day_07.py
@@ -7,8 +7,8 @@ sum_of_valid_results = 0
 
 sum_of_valid_results = 0
 
-with open("input/07/test_input", "r") as file:
-    # with open("input/07/real_input", "r") as file:
+with open("input/07/real_input", "r") as file:
+    # with open("input/07/test_input", "r") as file:
     for line in file:
         print()
         # print(line)
@@ -17,7 +17,7 @@ with open("input/07/test_input", "r") as file:
         terms = [int(term) for term in equation.strip().split(" ")]
         print(f"Target Result: {target_result}, terms: {terms}")
         num_operations = len(terms) - 1
-        possible_operators = ["+", "*"]
+        possible_operators = ["+", "*", "||"]
         operator_combinations = list(
             itertools.product(possible_operators, repeat=num_operations)
         )
@@ -32,6 +32,8 @@ with open("input/07/test_input", "r") as file:
                     first_operand += terms[i + 1]
                 elif operators[i] == "*":
                     first_operand *= terms[i + 1]
+                elif operators[i] == "||":
+                    first_operand = int(str(first_operand) + str(terms[i + 1]))
             print(f"Result: {first_operand}")
             if first_operand == target_result:
                 sum_of_valid_results += target_result
